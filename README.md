aa67b7ac1aaaa699f5dc9c5695cc5d9a6ff2f4d5
mdurrant <mdurrant@expressmedia.com>
2015-03-18 01:15:07 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# php-console-color
-Extremely simple library for writing to the console in color
+# PhpConsoleColor
+
+####Purpose
+This lightweight library only serves to make formatted console output code more readable.
+
+Instead of:
+```php
+echo "\033[0;31Foo:\031[0m Bar";
+```
+We can do:
+```php
+$console->writeLn("<red>Foo:</red> Bar");
+```
+
+####Usage
+```php
+$console = new Console();
+$console->write('This');
+$console->write('is');
+$console->write('a');
+$console->write('<blue>Test</blue>');
+$console->writeLn('Hello world!');
+```
+will output
+
+![Example output](http://i.imgur.com/jmJe8vu.png)
+
+####License
+MIT
---
+++ src/Console.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace PhpConsoleColor;
+
+class Console
+{
+    /**
+     * @var array
+     */
+    protected $map = [
+        'black'     => '0;30m',
+        'red'       => '0;31m',
+        'green'     => '0;32m',
+        'yellow'    => '0;33m',
+        'blue'      => '0;34m',
+        'purple'    => '0;35m',
+        'cyan'      => '0;36m',
+        'white'     => '0;37m',
+
+        'blackB'    => '1;30m',
+        'redB'      => '1;31m',
+        'greenB'    => '1;32m',
+        'yellowB'   => '1;33m',
+        'blueB'     => '1;34m',
+        'purpleB'   => '1;35m',
+        'cyanB'     => '1;36m',
+        'whiteB'    => '1;37m',
+
+        'blackU'    => '4;30m',
+        'redU'      => '4;31m',
+        'greenU'    => '4;32m',
+        'yellowU'   => '4;33m',
+        'blueU'     => '4;34m',
+        'purpleU'   => '4;35m',
+        'cyanU'     => '4;36m',
+        'whiteU'    => '4;37m',
+    ];
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    private function format($str)
+    {
+        preg_match_all('/<(\/?\w+)>/i', $str, $matches);
+        if (empty($matches[1])) {
+            return $str;
+        }
+
+        foreach ($matches[1] as $k => $match) {
+            if (strpos($match, '/') === 0) {
+                $str = str_replace($matches[0][$k], "\033[0m", $str);
+                continue;
+            }
+
+            if (!empty($this->map[$match])) {
+                $str = str_replace($matches[0][$k], "\033[" . $this->map[$match], $str);
+            }
+        }
+
+        return $str;
+    }
+
+    /**
+     * @param $str
+     */
+    public function write($str)
+    {
+        print $this->format($str);
+    }
+
+    /**
+     * @param string|null $str
+     */
+    public function writeLn($str = null)
+    {
+        $this->write("\n" . $str);
+    }
+}
